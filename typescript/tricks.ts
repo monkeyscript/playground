e49ce71945f5b1d1582a09f3f9552d367bb8273f
MonkeyScript <monkeyscript.dev@gmail.com>
2020-04-04 21:40:41 +0530

    TS tricks

--- typescript/tricks.ts
+++ typescript/tricks.ts
@@ -8,13 +8,50 @@ function fetchRandomItem(array: any[]): any {
 //
 // Generate random hex color
 //
-function generateRandomHexColor(){
-    return Math.floor(Math.random()*16777215).toString(16);
+function generateRandomHexColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
 }
 
 //
 // Remove last character from a string
 //
-function trimLastCharacter(str:string):string{
-    return str.substring(0,str.length-1);
+function trimLastCharacter(str: string): string {
+    return str.substring(0, str.length - 1);
+}
+
+//
+// Number to string
+//
+function convertNumberToString(num: number): string {
+    return num + "";
+}
+
+//
+// String to number
+//
+function convertStringToNumber(str: string): number {
+    return +str;
+}
+
+//
+// Float to int
+//
+function convertFloatToInt(num: number): number {
+    const int = num | 0;
+    return int;
+}
+
+//
+// Filter unique values
+//
+function filterUniqueValues(array: any[]): any[] {
+    return [...new Set(array)];
+}
+
+//
+// Trim array
+//
+function trimArray(array: any[], n: number): any[] {
+    array.length = n;
+    return array;
 }
