75f7ef6956a9f04f7cd62463e3ab827a71551394
MonkeyScript <monkeyscript.dev@gmail.com>
2020-05-08 22:41:18 +0530

    Update git commands

--- shell/git_commands.sh
+++ shell/git_commands.sh
@@ -1,3 +1,36 @@
+# Checks the git version
+git --version
+
+# Set system level configuration
+git config --system
+
+# Set user level configuration
+git config --global
+
+# Set project level configuration
+git config
+
+# List saved configuration
+git config --list
+
+# View entire git log
+git log
+
+# View git log : limit by count
+git log -n <count>
+
+# View git log : limit by time
+git log --since=<time>
+
+# View git log : limit by time
+git log --until=<time>
+
+# View git log : limit by author
+git log --author=<name>
+
+# View git log : limit by commit message
+git log --grep=<regex>
+
 # Adds changes to stage/index
 git add .
 
@@ -6,6 +39,15 @@ git commit -m "Your message"
 
 # Checks the status of files you have changed in your working directory
 git status
+
+# Shows diff btw working copy and repo
+git diff
+
+# Shows diff btw staging and repo
+git diff --staged
+
+# Shows diff highlight changes only
+git diff --color-words
 
 # Lists out all branches
 git branch
@@ -18,3 +60,24 @@ git push/pull <remote> <branch>
 
 # Switch to another branch
 git checkout <branch>
+
+# Shows changes in a commit
+git show <id>
+
+# Compares two commits
+git diff <id>...<id>
+
+# Unstage changes
+git reset HEAD <file>
+
+# Discard all changes
+git checkout --
+
+# Amend to most recent commit
+git commit --amend -m "message"
+
+# Reverts a commit
+git revert <id>
+
+# Removes untracked files
+git clean -f
